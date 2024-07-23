6000b4ef6241085109e9240a754098deb47add40
Rianne VG <85849537+riannevg@users.noreply.github.com>
2024-07-23 16:52:34 -0600

    js update

--- scripts.js
+++ scripts.js
@@ -4,26 +4,26 @@
 $( document ).ready(function() {
     
 
-    // This is the hamburger menu
-    $('.hamburger').click(function(){
+    // // This is the hamburger menu
+    // $('.hamburger').click(function(){
 
-        $(this).toggleClass('active');
-        $('.mobile-menu').fadeToggle();
+    //     $(this).toggleClass('active');
+    //     $('.mobile-menu').fadeToggle();
 
-    });
-
-
-
-    // This is for animate on scroll to initiate
-    AOS.init();
+    // });
 
 
-    // Slick Slider
 
-    $('.slider-container').slick({
-        autoplay:true,
-        autoplaySpeed:3000,
-        speed:1500,
-        arrows:false,
-        dots:true
-      });
+    // // This is for animate on scroll to initiate
+    // AOS.init();
+
+
+    // // Slick Slider
+
+    // $('.slider-container').slick({
+    //     autoplay:true,
+    //     autoplaySpeed:3000,
+    //     speed:1500,
+    //     arrows:false,
+    //     dots:true
+    //   });
