444f43e791d73fefcfaad7f017c639cff04e131c
Rianne VG <85849537+riannevg@users.noreply.github.com>
2024-08-12 19:02:45 -0600

    hamburger

--- scripts.js
+++ scripts.js
@@ -1,12 +1,11 @@
 // A $( document ).ready() block.
 // This is to initiate Javascript. Always has to go on top. 
-$( document ).ready(function() {
-    
+   
 
-    // This is the hamburger menu
-    $('.hamburger').click(function(){
-
-        $(this).toggleClass('active');
-        $('.mobile-menu').fadeToggle();
-
+    $(document).ready(function() {
+        $('.hamburger').click(function() {
+            $(this).toggleClass('active');
+            $('.mobile-menu').fadeToggle();
+        });
     });
+    
