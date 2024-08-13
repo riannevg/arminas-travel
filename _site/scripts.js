c45b6cde9a96c2dca3feccfe53b700dc9cb5208b
Rianne VG <85849537+riannevg@users.noreply.github.com>
2024-08-12 18:49:46 -0600

    jquery hamburger

--- _site/scripts.js
+++ _site/scripts.js
@@ -1,8 +1,8 @@
 // A $( document ).ready() block.
 // This is to initiate Javascript. Always has to go on top. 
-
 $( document ).ready(function() {
     
+
     // This is the hamburger menu
     $('.hamburger').click(function(){
 
