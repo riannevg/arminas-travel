ca97898f225893aa35f68b415334a46fe08d7994
Rianne VG <85849537+riannevg@users.noreply.github.com>
2024-08-12 18:44:29 -0600

    hamburger menu

--- _site/scripts.js
+++ _site/scripts.js
@@ -3,27 +3,10 @@
 
 $( document ).ready(function() {
     
+    // This is the hamburger menu
+    $('.hamburger').click(function(){
 
-    // // This is the hamburger menu
-    // $('.hamburger').click(function(){
+        $(this).toggleClass('active');
+        $('.mobile-menu').fadeToggle();
 
-    //     $(this).toggleClass('active');
-    //     $('.mobile-menu').fadeToggle();
-
-    // });
-
-
-
-    // // This is for animate on scroll to initiate
-    // AOS.init();
-
-
-    // // Slick Slider
-
-    // $('.slider-container').slick({
-    //     autoplay:true,
-    //     autoplaySpeed:3000,
-    //     speed:1500,
-    //     arrows:false,
-    //     dots:true
-    //   });
+    });
